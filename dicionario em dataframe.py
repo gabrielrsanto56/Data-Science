4c23ec51788a242acc9c94f2f4887159a90311ca
Gabriel R. Santos <gabrielrsantos999@gmail.com>
2025-04-03 16:52:46 -0300

    retirando os arquivos do ambiente virtual

--- dicionario em dataframe.py
+++ dicionario em dataframe.py
@@ -1,11 +1,11 @@
 import pandas as pd
 dic = {
     'nome': ['lucas', 'joao', 'maria', 'jose'],
-    'vedas': [1500, 2000, 1700, 2200],
+    'vendas': [1500, 2000, 1700, 2200],
     'comissao': [0.05, 0.07, 0.04, 0.08]
     }
 dic_dados = pd.DataFrame(dic)
 print(dic_dados)
 print(dic_dados.isnull().sum())
-print(dic_dados[dic_dados['vedas']>1700])
-print(dic_dados.vedas)
+print(dic_dados[dic_dados['vendas']>1700])
+print(dic_dados.vendas)
